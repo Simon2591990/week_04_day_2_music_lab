f988cdf593f3f79552714691abcf41cfcb08d2a0
Simon Charters <simon259@hotmail.co.uk>
2020-12-08 18:49:48 +0000

    MVP done

--- console.py
+++ console.py
@@ -1,17 +1,24 @@
 import pdb
 from models.artist import Artist 
-
+from models.album import Album
+import repositories.album_repository as album_repository
 import repositories.artist_repository as artist_repository
 
 artist_1 = Artist("Jimi Hendrix")
 artist_2 = Artist("Pink FLoyd")
+album_1 = Album("Axis: Bold as Love", "rock", artist_1)
 
 #delete all
+album_repository.delete_all()
 artist_repository.delete_all()
 
 artist_repository.save(artist_1)
 artist_repository.save(artist_2)
 
-# artist_repository.select_all()
+artist_repository.select_all()
 # artist_repository.select(1)
 # artist_repository.delete(4)
+album_repository.save(album_1)
+
+
+pdb.set_trace()
